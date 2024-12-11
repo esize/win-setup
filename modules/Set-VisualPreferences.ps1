d700d3046185031b2ea0c52fed435e5d7cb18454
Evan Sizemore <evan@wool.homes>
2024-12-11 13:40:34 -0500

    Remove cursor theme specific settings, now handled by default theme.

--- modules/Set-VisualPreferences.ps1
+++ modules/Set-VisualPreferences.ps1
@@ -77,35 +77,4 @@ public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wP
         Write-Log "Theme pack file not found at: $themePath" -Level Warning
     }
 
-    # Apply cursor scheme
-    if ($settings.visual.cursorScheme -eq "Windows Black") {
-        $cursorPath = "HKCU:\Control Panel\Cursors"
-        $blackCursorScheme = @{
-            "Arrow"       = "%SystemRoot%\cursors\aero_arrow.cur"
-            "Hand"        = "%SystemRoot%\cursors\aero_link.cur"
-            "IBeam"       = "%SystemRoot%\cursors\beam_r.cur"
-            "Wait"        = "%SystemRoot%\cursors\busy_r.cur"
-            "SizeNWSE"    = "%SystemRoot%\cursors\size1_r.cur"
-            "SizeNESW"    = "%SystemRoot%\cursors\size2_r.cur"
-            "SizeWE"      = "%SystemRoot%\cursors\size3_r.cur"
-            "SizeNS"      = "%SystemRoot%\cursors\size4_r.cur"
-            "SizeAll"     = "%SystemRoot%\cursors\move_r.cur"
-            "No"          = "%SystemRoot%\cursors\unavail_r.cur"
-            "AppStarting" = "%SystemRoot%\cursors\working_r.cur"
-            "Help"        = "%SystemRoot%\cursors\help_r.cur"
-            "Scheme"      = "Windows Black"
-        }
-
-        foreach ($cursor in $blackCursorScheme.GetEnumerator()) {
-            Set-ItemProperty -Path $cursorPath -Name $cursor.Key -Value $cursor.Value
-        }
-
-        # Refresh cursor settings
-        $signature = @'
-[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
-public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
-'@
-        $refresh = Add-Type -MemberDefinition $signature -Name WinAPI -Namespace SystemParamInfo -PassThru
-        $refresh::SystemParametersInfo(0x0057, 0, $null, 0x01)
-    }
 } 
