bbdb4c07107a97a47b9084185e4537141ba804f0
Evan Sizemore <evan@wool.homes>
2024-12-12 14:28:35 -0500

    Remove outlook better

--- modules/Remove-DefaultApps.ps1
+++ modules/Remove-DefaultApps.ps1
@@ -2,8 +2,6 @@ function Remove-DefaultApps {
     [CmdletBinding()]
     param()
 
-    Write-InfoLog "Removing default Windows applications..."
-    
     $appsToRemove = @(
         "Microsoft.Windows.Copilot",
         "Microsoft.WindowsFeedbackHub",
@@ -34,7 +32,18 @@ function Remove-DefaultApps {
         try {
             $appExists = Get-AppxPackage -Name $app -AllUsers -ErrorAction SilentlyContinue
             if ($appExists) {
-                Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -ErrorAction Stop
+                # Stop related processes first
+                if ($app -eq "Microsoft.OutlookForWindows") {
+                    Get-Process | Where-Object { $_.Name -like "*outlook*" } | Stop-Process -Force -ErrorAction SilentlyContinue
+                    Start-Sleep -Seconds 2
+                }
+
+                # Try to remove provisioned package first
+                Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq $app } | 
+                    Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+
+                # Remove the package for all users
+                Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction Stop
                 Write-VerboseLog "Successfully removed $app"
             } else {
                 Write-VerboseLog "$app not found, skipping..."
@@ -42,6 +51,20 @@ function Remove-DefaultApps {
         }
         catch {
             Write-WarningLog "Failed to remove $app : $_"
+            
+            # Additional cleanup attempt for Outlook
+            if ($app -eq "Microsoft.OutlookForWindows") {
+                try {
+                    # Force remove using DISM
+                    $packageFullName = (Get-AppxPackage -Name $app -AllUsers).PackageFullName
+                    if ($packageFullName) {
+                        DISM.exe /Online /Remove-ProvisionedAppxPackage /PackageName:$packageFullName
+                    }
+                }
+                catch {
+                    Write-WarningLog "Failed additional cleanup for Outlook: $_"
+                }
+            }
         }
     }
 } 
