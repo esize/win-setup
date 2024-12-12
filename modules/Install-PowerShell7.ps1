146910d32ef0862a02abf4153dad6b169190da91
Evan Sizemore <evan@wool.homes>
2024-12-12 00:08:36 -0500

    Simplify PS7 install

--- modules/Install-PowerShell7.ps1
+++ modules/Install-PowerShell7.ps1
@@ -26,28 +26,4 @@ function Install-PowerShell7 {
     else {
         Write-Log "PowerShell 7 is already installed."
     }
-
-    # Get the current PowerShell version
-    $currentVersion = $PSVersionTable.PSVersion.Major
-    
-    # Only restart in PowerShell 7 if we're running in an older version
-    if ($currentVersion -lt 7) {
-        Write-Log "Restarting script in PowerShell 7..."
-        
-        # Save current script path and arguments
-        $scriptPath = $MyInvocation.MyCommand.Path
-        $arguments = $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { "-$($_.Key) `"$($_.Value)`"" }
-        
-        # Change directory to avoid file lock issues
-        Set-Location $env:USERPROFILE
-        
-        # Start PowerShell 7 with the script
-        $process = Start-Process pwsh -ArgumentList "-NoExit -File `"$scriptPath`" $arguments" -PassThru -Wait
-        
-        # Exit the current PowerShell session
-        exit $process.ExitCode
-    }
-    else {
-        Write-Log "Already running in PowerShell 7, continuing..."
-    }
-} 
+}
